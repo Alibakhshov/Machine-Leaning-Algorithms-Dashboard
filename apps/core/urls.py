2f1b5e605b627f5a2f5ce2ca33b5277ec0dc1937
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2024-05-10 13:28:22 +0600

    Minor change

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('algorithms/', views.algorithms, name='algorithms'),
+    path('', views.algorithms, name='algorithms'),
     path('simple-linear-regression/', views.simple_linear_regression, name='simple_linear_regression'),
     path('multiple-linear-regression/', views.multiple_linear_regression, name='multiple_linear_regression'),
     path('k-nearest-neighbor/', views.k_nearest_neighbor, name='k_nearest_neighbor'),
